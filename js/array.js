8108ece9434151199bf7001be1cfc54a0853fbd1
Doit <duhee7524@naver.com>
2024-05-14 14:43:08 +0900

    배열, 함수
    배열, 함수

---
+++ js/array.js
@@ -0,0 +1,20 @@
+
+// 배열 순회 
+
+let data = [1, 2, 3];
+
+for (let i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
+
+data.forEach(function(value) {
+    console.log("value is", value);
+});
+
+for (let value of data) {
+    console.log(value);
+}
+
+for (let idx in data) {
+  console.log(data[idx]);
+}
---
+++ js/function.js
@@ -0,0 +1,11 @@
+function safelyGet(obj,name){
+
+}
+
+const safelyGet = function(obj, name) {
+   
+};
+
+const safelyGet = (obj,name) => {
+    
+}
